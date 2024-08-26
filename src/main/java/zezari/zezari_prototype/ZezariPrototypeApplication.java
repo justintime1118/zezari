47706f02ea40441ee86585111cb2f66c07d3fed6
justinYoo <ugh1118@gmail.com>
2024-08-26 13:30:16 +0900

    [init] additional init

---
+++ src/main/java/zezari/zezari_prototype/ZezariPrototypeApplication.java
@@ -0,0 +1,13 @@
+package zezari.zezari_prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZezariPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZezariPrototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zezari/zezari_prototype/ZezariPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package zezari.zezari_prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZezariPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
